9b3d9ee97d26421f8a5a8896a21b588fb1503d1d
Caroline <145659306+CarotteT@users.noreply.github.com>
2024-03-23 16:30:48 +0100

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,27 @@
+INSERT INTO genre (nom) VALUES ('Action');
+INSERT INTO genre (nom) VALUES ('Comédie');
+INSERT INTO genre (nom) VALUES ('Drame');
+INSERT INTO genre (nom) VALUES ('Horreur');
+INSERT INTO genre (nom) VALUES ('Science-fiction');
+INSERT INTO genre (nom) VALUES ('Thriller');
+
+INSERT INTO auteur (nom, prenom) VALUES ('Tarantino', 'Quentin');
+INSERT INTO auteur (nom, prenom) VALUES ('Nolan', 'Christopher');
+INSERT INTO auteur (nom, prenom) VALUES ('Spielberg', 'Steven');
+INSERT INTO auteur (nom, prenom) VALUES ('Scorsese', 'Martin');
+INSERT INTO auteur (nom, prenom) VALUES ('Coppola', 'Francis Ford');
+INSERT INTO auteur (nom, prenom) VALUES ('Kubrick', 'Stanley');
+
+INSERT INTO acteur (nom, prenom) VALUES ('DiCaprio', 'Leonardo');
+INSERT INTO acteur (nom, prenom) VALUES ('Pitt', 'Brad');
+INSERT INTO acteur (nom, prenom) VALUES ('De Niro', 'Robert');
+INSERT INTO acteur (nom, prenom) VALUES ('Hanks', 'Tom');
+INSERT INTO acteur (nom, prenom) VALUES ('Cruise', 'Tom');
+
+INSERT INTO studio (nom) VALUES ('Warner Bros');
+INSERT INTO studio (nom) VALUES ('Universal Pictures');
+INSERT INTO studio (nom) VALUES ('Paramount Pictures');
+INSERT INTO studio (nom) VALUES ('20th Century Fox');
+INSERT INTO studio (nom) VALUES ('Columbia Pictures');
+INSERT INTO studio (nom) VALUES ('MGM');
+
