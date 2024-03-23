e370e3ee05ffcf40cdd7ed9d7e7287880228ca05
AlphaxHotelxMikexEchoxDelta <95902084+AlphaxHotelxMikexEchoxDelta@users.noreply.github.com>
2024-03-23 16:32:18 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,41 @@
 from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
+class Singleton:
+    __instance = None
+    @staticmethod 
+    def getInstance():
+        if Singleton.__instance == None:
+            Singleton()
+        return Singleton.__instance
+    def __init__(self):
+        if Singleton.__instance != None:
+            raise Exception("Cette class est un singleton!")
+        else:
+            Singleton.__instance = self
+            self.connection = mysql.connector.connect(
+                host="localhost",
+                user="films",
+                password="films",
+                database="films"
+            )
+            self.cursor = self.connection.cursor()
+
+@app.route('/genres', methods=['POST'])
+def get_genres():
+    singleton = Singleton.getInstance()
+    singleton.cursor.execute("SELECT * FROM genre")
+    result = singleton.cursor.fetchall()
+    return jsonify(result)
+
+@app.route('/studios', methods=['POST'])
+def get_studios():
+    singleton = Singleton.getInstance()
+    singleton.cursor.execute("SELECT * FROM studio")
+    result = singleton.cursor.fetchall()
+    return jsonify(result)
 
 @app.route('/films', methods=['POST'])
 def get_films():
